9467d1b4e4af1688ac57c4a3f7d2cd7574d7abe3
Muhammad_Adnan <madnan2014@namal.edu.pk>
2016-11-15 11:36:17 +0500

    SD-1 Problems Telling(Syntax,Runtime,Logic Errors)

---
+++ HelloWorld.java
@@ -0,0 +1,85 @@
+   ******************************************* 
+                 "Question No : 1"
+
+public class HelloWorld{
+	public static void main(String[] args){
+		int    a=15;
+		if (a==15)
+		{
+			System.out.print("asds");
+		}
+	}
+}
+
+/*Here the error is syntax error(Incorrect format in selection),As in line 3 there is a lot of
+  distance between int and a.It's as int a=15; */
+
+
+
+    *******************************************
+		"Question No :2"
+
+
+
+public class HelloWorld{
+	public static void main(String[] args)
+	{
+			System.out.print(100/0);
+
+	}
+}
+
+
+/*No compilation Error,Run time error because any number devided by zero then program will crash and logic error also
+ Because if i m dividing 100 by some number e.g 10,30 it will be divided but in case of zero it would'nt be.*/
+
+    ********************************************
+                   "Question No : 3 "
+
+public class HelloWorld{
+	public static void main(String[] args)
+	{
+			Sytem.out.println(100/100);
+
+	}
+}
+
+/*Syntax error in Third line because it's written as  sytem.out.println(100/100);  while it must be system.out.println(100/100);*/
+
+     *******************************************
+                  "QUestion NO : 4 "
+  
+
+public class HelloWorld{
+	public static void main(String[] args)
+	{
+		int a,b=15;
+		Sytem.out.println(a+"  "+b);
+
+	}
+}
+
+/*Here a must be initialized with any value so its a Syntax error.*/
+
+      *******************************************
+                   "Question No :5"
+
+
+
+public class HelloWorld{
+	public void main(String[] args)
+	{
+		int a=15;
+		int b=16;
+		c=a+b;
+		System.out.println(c);
+
+	}
+}
+
+/*Here static is missing so it's a run time error and also It's a syntax error because c is not initialized*/
+  
+     *********************************************
+
+
+
